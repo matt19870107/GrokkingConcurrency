12b5516bd38ab0629bef238223c50afb20f9398e
matt19870107 <735806197@qq.com>
2023-02-02 13:24:52 +0800

    update

--- src/test/java/com/matt/homework1/UnSafeSequenceTest.java
+++ src/test/java/com/matt/homework1/UnSafeSequenceTest.java
@@ -17,36 +17,24 @@ public class UnSafeSequenceTest {
 
         UnsafeSequence sequence = new UnsafeSequence();
 
-        CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(() -> {
-            int value = 0;
-            for(int i=1; i <= 20; i++) {
-                value = sequence.getNext();
-                try {
-                    Thread.sleep(100);// Do something
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return value;
-        });
-
-        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(() -> {
-            int value = 0;
-            for(int i=1; i <= 20; i++) {
-                value = sequence.getNext();
-                try {
-                    Thread.sleep(100);// Do something
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return value;
-        });
-        List<CompletableFuture<Integer>> futures = List.of(future1, future2);
+        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> increase20Times(sequence));
+        CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> increase20Times(sequence));
+        List<CompletableFuture<Void>> futures = List.of(future1, future2);
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
 
         assertNotEquals(40, sequence.getValue());
+    }
+
+    private void increase20Times(UnsafeSequence sequence){
+        int value = 0;
+        for(int i=1; i <= 20; i++) {
+            value = sequence.getNext();
+            try {
+                Thread.sleep(100);// Do something
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
